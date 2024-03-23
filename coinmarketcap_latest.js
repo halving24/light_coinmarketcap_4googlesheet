b0177b9c93affe21540a9a010a61b1bc7c8bd5e4
halving24 <160751362+halving24@users.noreply.github.com>
2024-03-23 09:05:06 +0100

    Update coinmarketcap_latest.js

--- coinmarketcap_latest.js
+++ coinmarketcap_latest.js
@@ -5,7 +5,7 @@ function get_cmc_list() {
   let API_KEY = sheet.getRange('B1').getDisplayValue().trim(); // get your API KEY from sheet
 
   // prepare url
-  let URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=" + 10 
+  let URL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=" + 10 // change 10 to get more tokens (max 5000)
   let options = {headers:{'X-CMC_PRO_API_KEY': String(API_KEY),'Content-Type': 'application/json'},};
 
   // fetch data from coinmarketcap json-api
@@ -14,7 +14,7 @@ function get_cmc_list() {
   // parse json response
   let tokens = JSON.parse(response).data
 
-  // Write token values to the sheet, starting from row = r
+  // Write token values to the sheet, starting from row = 4+x
     for (let x=0; x<tokens.length; x++){
     sheet.getRange(4+x, 1).setValue(tokens[x].symbol); // replace 4 by your starting row
     sheet.getRange(4+x, 2).setValue(tokens[x].name);
