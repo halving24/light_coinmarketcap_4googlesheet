8e7485cf0ce17b311dd75576bcfe7b6f17c1323a
halving24 <160751362+halving24@users.noreply.github.com>
2024-03-23 09:32:39 +0100

    Update getmytokens_list.js

--- getmytokens_list.js
+++ getmytokens_list.js
@@ -1,5 +1,5 @@
-function get_mytokens_list() {
-  var mysheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+function get_mytokens_list() {  
+  var mysheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet(); // function is executed on the ActiveSheet, so be careful if you use inside another function
   Logger.log("sheet " + mysheet.getSheetName())
   const rs = 8, cs = 2 // position of your token list starting cell in your sheet (rs=rowstart, cs=colstart)
   var rl = mysheet.getLastRow()-(rs-1)
